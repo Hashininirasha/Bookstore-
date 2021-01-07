e58b71c00ebe572ed10cb7be3bdbe5eaec2cbb96
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-07 17:05:01 +0530

    Added Username and pic

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <title>
         Book Store
     </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <!--style-->
 <link rel="stylesheet" href="style.css">
@@ -25,7 +27,7 @@
             <form action="" method="POST" class="w-50">
                 <div class="py-2">
                     <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">@</span>
+                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-id-badge" aria-hidden="true"></i></span>
                     <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
                     </div>
                 </div>
