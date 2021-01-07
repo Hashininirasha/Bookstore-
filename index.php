036d09d1ff84788889ddbb499bfb49e33621dfb1
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-07 17:08:51 +0530

    gave infocolor to icon

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             <form action="" method="POST" class="w-50">
                 <div class="py-2">
                     <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1"><i class="fa fa-id-badge" aria-hidden="true"></i></span>
+                    <span class="input-group-text bg-info" id="basic-addon1"><i class="fa fa-id-badge" aria-hidden="true"></i></span>
                     <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
                     </div>
                 </div>
