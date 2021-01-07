ea18b5c5fe0ca0dab34fcdb577a3c0b26c4a4d3e
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-07 17:10:17 +0530

    added place holder

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                 <div class="py-2">
                     <div class="input-group mb-3">
                     <span class="input-group-text bg-info" id="basic-addon1"><i class="fa fa-id-badge" aria-hidden="true"></i></span>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                    <input type="text" autocomplete="off" placeholder="ID" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
                     </div>
                 </div>
             </form>
