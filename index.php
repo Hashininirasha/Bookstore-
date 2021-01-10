d900bfb56c12b0cc4219326939c60b7e70a178ed
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-10 15:59:03 +0530

    added bootsrap

--- index.php
+++ index.php
@@ -3,24 +3,28 @@ require_once("../BOOK_STORE/php/Component.php");
 
 
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport"
-    content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimun-scale=1.0"
-    >
+    content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimun-scale=1.0">
     <meta http-equiv='X-UA-Compatible' content="ie=edge">
     <title>
         Book Store
     </title>
+    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-<!--style-->
-<link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
 </head>
     <body>
+    <h1>The 2018 Top Programming Languages</h1>
     <main>
         <div class="container text-center">
         <div class="p-3 mb-2 bg-danger text-white">
@@ -75,6 +79,7 @@ require_once("../BOOK_STORE/php/Component.php");
             </form>
         </div>
         <!--Bootstrap Table-->
+        <div class="d-flex table-data">
         <table class="table table-striped table-dark">
             <thead>
             <tr>
@@ -86,6 +91,8 @@ require_once("../BOOK_STORE/php/Component.php");
             </tr>
             </thead>
         </table>
+        </div>
+    </div>
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
